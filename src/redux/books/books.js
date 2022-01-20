f24b247864349b1f43c84e9d629ae4c0e146f023
rtonata88 <rtonata@gmail.com>
2022-01-20 21:10:21 +0200

    Connect API

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,7 +1,12 @@
+import { createAsyncThunk } from 'react-redux';
+
+const GET_BOOKS = 'bookStore/books/GET_BOOKS';
 const ADD_BOOK = 'bookStore/books/ADD_BOOK';
 const REMOVE_BOOK = 'bookStore/books/REMOVE_BOOK';
 
 const initialState = [];
+
+const getBooksFromApi = createAsyncThunk();
 
 export const addBook = (payload) => ({
   type: ADD_BOOK,
