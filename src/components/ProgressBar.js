2d45333fb134ff9bcb658c74897f1fe037968cba
rtonata88 <rtonata@gmail.com>
2022-01-22 19:22:58 +0200

    Fix linter errors

--- src/components/ProgressBar.js
+++ src/components/ProgressBar.js
@@ -5,8 +5,8 @@ const ProgressBar = () => (
   <div className="d-flex justify-content-start align-items-center">
     <img src={progress} alt="Progress" className="img-fluid w-25" />
     <div>
-      <p className="display-6">10%</p>
-      <p>Completed</p>
+      <p className="progress-percentage">10%</p>
+      <p className="progress-status">Completed</p>
     </div>
   </div>
 );
